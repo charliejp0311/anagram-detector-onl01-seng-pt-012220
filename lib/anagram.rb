49303ada94377858631f26fbcef6b119761f4f5c
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 14:33:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,11 @@
 # Your code goes here!
 require "pry"
+class Anagram
+  attr_accessor :word_to_compare :array_to_compare_to
+
+  def initialize(w, a)
+    @word_to_compare = w 
+    @array_to_compare_to = a 
+  end
+  
+end
