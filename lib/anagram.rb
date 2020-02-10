ae65d1e91b831ed2d51e2d9cc9e51d64c171bf2a
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 14:38:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,9 +3,9 @@ require "pry"
 class Anagram
   attr_accessor :word_to_compare, :array_to_compare_to
 
-  def initialize(w, a)
+  def initialize(w)
     @word_to_compare = w
-    @array_to_compare_to = a
+    #@array_to_compare_to = a
     binding.pry
   end
 
