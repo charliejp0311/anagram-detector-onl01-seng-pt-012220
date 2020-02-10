d19914c8dcc3b17d2e5c5c4f2ddbb83652ccc3ad
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 14:33:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,8 +4,8 @@ class Anagram
   attr_accessor :word_to_compare :array_to_compare_to
 
   def initialize(w, a)
-    @word_to_compare = w 
-    @array_to_compare_to = a 
+    @word_to_compare = w
+    @array_to_compare_to = a
   end
-  
+
 end
