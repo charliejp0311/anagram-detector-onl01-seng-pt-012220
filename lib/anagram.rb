9965360fb594acf9094447a278729dc3191009c5
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 15:12:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,21 @@ class Anagram
     @word_to_compare = w
     #binding.pry
   end
+  def split_word_to_array(each_word)
+    each_word.split("")
+  end
   def match(a)
+    @all_matches = []
     @array_to_compare_to = a
 
+    @array_to_compare_to.each do |word|
+
+        if split_word_to_array(@word_to_compare).sort == split_word_to_array(word)
+          @all_matches << word           
+        end
+        
+    end
+    @all_matches
   end
 
 end
